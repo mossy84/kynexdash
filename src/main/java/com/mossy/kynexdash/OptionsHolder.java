36a17d145124a355723ffa627cb49269d3a64788
mossy84 <71836363+mossy84@users.noreply.github.com>
2024-02-24 08:42:14 -0500

    Update OptionsHolder.java
    added option to disable dash for survival mode

--- src/main/java/com/mossy/kynexdash/OptionsHolder.java
+++ src/main/java/com/mossy/kynexdash/OptionsHolder.java
@@ -8,12 +8,14 @@ public class OptionsHolder {
 	
 	public static final ForgeConfigSpec.ConfigValue<Double> BOOST_VELOCITY;
 	public static final ForgeConfigSpec.ConfigValue<Integer> COOLDOWN;
+	public static final ForgeConfigSpec.ConfigValue<Boolean> ALLOWED_IN_SURVIVAL;
 	
 	static {
 		BUILDER.push("Config for KynexDash");
 		
 		BOOST_VELOCITY = BUILDER.comment("Sets the boost velocity. Default: 2.0D").define("Boost Velocity", 2.0D);
 		COOLDOWN = BUILDER.comment("Sets the cooldown. Default: 60").define("Cooldown", 60);
+		ALLOWED_IN_SURVIVAL = BUILDER.comment("Can the dash be used in survival").define("Allowed in survival", true);
 		
 		BUILDER.pop();
 		SPEC = BUILDER.build();
